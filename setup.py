1c643b93899f44557e192c4c4d602d2e95710e18
jeromaerts <42966049+jeromaerts@users.noreply.github.com>
2018-10-12 17:29:38 +0200

    toymodel_lumped
    setup.py added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='toymodel_lumped_bmi',
+    version='',
+    packages=['', 'toymodel_lumped'],
+    url='',
+    license='',
+    author='jerom',
+    author_email='',
+    description=''
+)
